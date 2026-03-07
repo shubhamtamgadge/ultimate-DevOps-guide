bfb75906775a9f0e86dbc10fac50d4d0f0bc98d1
Shubham Tamgadge <shubhamtamgadge80@gmail.com>
2026-03-07 18:43:49 +0530

    Update nexus-setup.sh

--- Nexus-and-SonarQube/nexus-setup.sh
+++ Nexus-and-SonarQube/nexus-setup.sh
@@ -12,16 +12,15 @@ echo "⬇️ Downloading Nexus Repository OSS 3.83.2-01..."
 cd /tmp
 wget https://download.sonatype.com/nexus/3/nexus-3.90.0-21-linux-x86_64.tar.gz -O nexus.tar.gz
 
-
 echo "📂 Extracting Nexus to /opt..."
 sudo tar -xvzf nexus.tar.gz -C /opt
-sudo ln -sfn /opt/nexus-3.83.2-01 /opt/nexus   # symlink for easy upgrades
+sudo ln -sfn /opt/nexus-3.90.0-21 /opt/nexus   # symlink for easy upgrades
 
 echo "👤 Creating nexus user..."
 id -u nexus &>/dev/null || sudo useradd nexus
 
 echo "📦 Setting ownership on Nexus installation..."
-sudo chown -R nexus:nexus /opt/nexus-3.83.2-01 /opt/nexus
+sudo chown -R nexus:nexus /opt/nexus-3.90.0-21 /opt/nexus
 
 echo "📦 Creating Nexus data directory..."
 sudo mkdir -p /opt/sonatype-work/nexus3
@@ -44,7 +43,7 @@ Group=nexus
 ExecStart=/opt/nexus/bin/nexus start
 ExecStop=/opt/nexus/bin/nexus stop
 Restart=on-abort
-Environment=JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
+Environment=JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto
 
 [Install]
 WantedBy=multi-user.target
@@ -61,4 +60,3 @@ sudo systemctl status nexus -l --no-pager
 echo "✅ Nexus Repository Manager installed successfully!"
 echo "👉 Access it at: http://<EC2-Public-IP>:8081"
 echo "🔐 Default login: admin / \$(cat /opt/sonatype-work/nexus3/admin.password)"
-
