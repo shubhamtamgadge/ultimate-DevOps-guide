14c66876821d207aee8433aa2d93b4abdd477c4d
Shubham Tamgadge <shubhamtamgadge80@gmail.com>
2026-03-08 16:01:59 +0530

    Create installation.sh

---
+++ terraform/installation.sh
@@ -0,0 +1,22 @@
+# go to google and search "terraform installation" ---> select the os type {if default amazon
+# select the amaxon linux 2023 ---> copy below commands}
+
+**switch to root user first**
+```bash
+sudo su - 
+```
+```bash
+sudo dnf install -y dnf-plugins-core
+
+```bash
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+```
+
+```bash
+sudo dnf -y install terraform
+```
+
+**check version**
+```bash
+terraform -v
+```
